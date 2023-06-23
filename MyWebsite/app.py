4e88937ea0f79dd6c880a1f19abfeea26649643a
Ostap1243223 <110120201+Ostap1243223@users.noreply.github.com>
2023-06-23 16:14:31 +0300

    Add files via upload

---
+++ MyWebsite/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from sql_queries import*
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    posts = get_posts()
+    category_list = get_categorys()
+    return render_template("index.html", category_list = category_list, posts = posts)
+    
+
+@app.route("/category/<id>")
+def category(id):
+    posts = get_category_post(id)
+    category_list = get_categorys()
+    category = get_category_name(id)[1].upper()
+    return render_template("category_posts.html", category_list = get_categorys(), posts = posts)
+
+@app.route("/post/<post_id>")
+def post(post_id):
+    post = get_post(post_id)
+    category_list = get_categorys()
+    return render_template("post.html",category_list = get_categorys(), post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+vanv/Scripts\activate
+python app.py
+"""
---
+++ MyWebsite/sql_queries.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+conn = sqlite3.connect("Artistc.db")
+cursor = conn.cursor()
+
+
+def open():
+    global conn, cursor
+    conn = sqlite3.connect("travel.db")
+    cursor = conn.cursor()
+    
+
+def close():
+    cursor.close()
+    conn.close()
+    
+def add_category(data):
+    open()
+    cursor.execute('''
+    INSERT INTO category (name) VALUES((?))''', [data])
+    conn.commit()
+    close()
+
+def add_user(*data):
+    open()
+    cursor.execute('''
+    INSERT INTO users (name, email, password) VALUES((?),(?),(?))
+    ''', [data[0], data[1], data[2]] )
+    conn.commit()
+    close()
+
+def get_post(post_id):
+    open()
+    cursor.execute('''SELECT * from posts WHERE id == (?)''', [post_id])
+    post = cursor.fetchone()
+    conn.commit()
+    close()
+    return post
+
+def get_category_name(id):
+    open()
+    cursor.execute('''SELECT * from category WHERE id == (?)''', [id])
+    category = cursor.fetchone()
+    conn.commit()
+    close()
+    return category
+
+def get_category_post(id):
+    open()
+    cursor.execute('''SELECT * from posts WHERE category_id == (?)''', [id])
+    post = cursor.fetchall()
+    conn.commit()
+    close()
+    return post
+
+def get_posts():
+    open()
+    cursor.execute('''SELECT * from posts''')
+    posts = cursor.fetchall()
+    conn.commit()
+    close()
+    return posts
+
+def get_categorys():
+    open()
+    cursor.execute('''SELECT * from category''')
+    data = cursor.fetchall()
+    conn.commit()
+    close()
+    return data
